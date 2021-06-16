909a36fa55e6deb4486407baa05c34d4167f7c9c
Nick <delunico@sheridancollege.ca>
2021-06-16 13:01:46 -0400

    remove jquery, fixed smooth scroll

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
-$(document).ready(function() {
-  
-    var scrollLink = $('.scroll');
-    
-    // Smooth scrolling
-    scrollLink.click(function(e) {
-      e.preventDefault();
-      $('html,body').animate({
-        scrollTop: $(this.hash).offset().top
-      }, 1000 );
-    });
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+  var scrollLinks = document.getElementsByClassName("scroll");
+
+  for(i=0;i<scrollLinks.length;i++) {
+    var scrollLink = scrollLinks[i];
+    scrollLink.addEventListener('click',function(e){
+        document.querySelector("."+e.target.id).scrollIntoView({ 
+          behavior: 'smooth' 
+        });
+      })
+  }
 });
+
+
+  
+  // document.querySelector('.projects').scrollIntoView({ 
+  //   behavior: 'smooth' 
+  // });
+  
+  // document.querySelector('.about').scrollIntoView({ 
+  //   behavior: 'smooth' 
+  // });
+  
+  // document.querySelector('.contact').scrollIntoView({ 
+  //   behavior: 'smooth' 
+  // });
+  
+
+
